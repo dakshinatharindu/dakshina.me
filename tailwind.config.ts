aa63049242f286bcbd97abdeddfe72dc6ed7dc70
dakshinatharindu <tharinduokd@gmail.com>
2024-11-10 23:56:27 +0530

    add daisyUI and Google font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,5 +14,13 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      "cupcake",
+      "dracula",
+    ],
+  },
 } satisfies Config;
