cc8ecc7e0033d15e48bfc362137a601dd3f15794
dakshinatharindu <tharinduokd@gmail.com>
2025-01-31 22:12:17 -0500

    revert back to previous

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,11 @@
-import Link from 'next/link';
-import {getSortedPostsData, PostData} from '@/lib/posts';
+import React from 'react'
 
-export default function Blog() {
-    const allPostsData: PostData[] = getSortedPostsData();
-
+const Blog = () => {
     return (
         <div>
             <h1>Blog</h1>
-            <ul>
-                {allPostsData.map(({id, title, date}) => (
-                    <li key={id}>
-                        <Link href={`/blog/${id}`}>{title}</Link>
-                        <br/>
-                        <small>{date}</small>
-                    </li>
-                ))}
-            </ul>
         </div>
-    );
+    )
 }
+
+export default Blog
